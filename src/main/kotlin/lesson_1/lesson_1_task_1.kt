1467a83e9fce0e934f75ef44b34d4790be4e3457
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-07 16:22:12 +0200

    KS-1-1 Print data from first lessom

--- src/main/kotlin/lesson_1/lesson_1_task_1.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_1.kt
@@ -1,7 +1,11 @@
 package lesson_1
 
 fun main() {
-
     println("Это решение задачи 1 из 1 урока")
+
+    val numberOfOrder: Int = 75
+    println("Количество заказов равно $numberOfOrder")
+    val thanksForBuying: String = "Спасибо за покупку!"
+    println("$thanksForBuying")
 
 }
