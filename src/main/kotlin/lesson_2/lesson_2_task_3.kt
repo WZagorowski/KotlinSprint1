aac5543774ad2d05e55b6380ed6c29e74d493179
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-09 20:25:19 +0200

    KS-2-3 Lesson 2 task 3

---
+++ src/main/kotlin/lesson_2/lesson_2_task_3.kt
@@ -0,0 +1,11 @@
+package lesson_2
+fun main(){
+    val hourOfDeparture: Int = 9
+    val minuteOfDeparture: Int = 39
+    val travelTime: Int = 457
+    val arrivalInMinutes = (hourOfDeparture * 60 + minuteOfDeparture + travelTime) % 1440
+    val hourOfArrival = arrivalInMinutes / 60
+    val minuteOfArrival = arrivalInMinutes % 60
+    val arrivalTime = String.format("$hourOfArrival:%02d",minuteOfArrival)
+    println("Arrival time: $arrivalTime")
+}
