6bee3f2009e4fce468bebeb8d6317e75c98a21bb
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-09 18:00:24 +0200

    KS-1-5 Lesson 1 task 5

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -1,9 +1,8 @@
 package lesson_1
 fun main(){
     val seconds = 6480
-    val minutes = (seconds / 60)
-    val modSeconds = (seconds % minutes).toDouble() / 100
-    val modSecondsStr = String.format("%.2f", modSeconds)
-
-    print("$minutes:" + modSecondsStr[2] + modSecondsStr[3])
+    val minutes = seconds / 60
+    val modSeconds = seconds % 60
+    val modSecondsStr = String.format("$minutes:%02d", modSeconds)
+    println(modSecondsStr)
 }
