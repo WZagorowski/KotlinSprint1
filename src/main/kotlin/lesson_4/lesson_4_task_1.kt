1b91706a7d60ab706d6842c1e5998deef6c33d32
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-11 21:22:36 +0200

    KS-4-1 Lesson 4 task 1

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -3,8 +3,9 @@ package lesson_4
 fun main(){
     val reservedToday = 13
     val reservedTomorrow = 9
-    val freeToday = (reservedToday < NUMBER_OF_TABLES)
-    val freeTomorrow = (reservedTomorrow < NUMBER_OF_TABLES)
+
+    val freeToday = reservedToday < NUMBER_OF_TABLES
+    val freeTomorrow = reservedTomorrow < NUMBER_OF_TABLES
     println("Доступность столиков на сегодня: $freeToday\nДоступность столиков на завтра: $freeTomorrow")
 }
 
