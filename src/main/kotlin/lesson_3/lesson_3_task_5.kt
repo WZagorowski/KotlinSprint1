f0ec946296c5c44dfcb1298439b03717f38a1076
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-11 22:16:30 +0200

    KS-3-5 Lesson 3 task 5

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -2,8 +2,11 @@ package lesson_3
 
 fun main(){
     val infoFromServer = "D2-D4;0"
-    val goFrom = "${infoFromServer[0]}${infoFromServer[1]}"
-    val goTo = "${infoFromServer[3]}${infoFromServer[4]}"
-    val moveNumber = "${infoFromServer[6]}"
+
+    val newInfo = infoFromServer.replace(";","-")
+    val result = newInfo.split("-")
+    val goFrom = result[0]
+    val goTo = result[1]
+    val moveNumber = result[2]
     println("$goFrom\n$goTo\n$moveNumber")
 }
