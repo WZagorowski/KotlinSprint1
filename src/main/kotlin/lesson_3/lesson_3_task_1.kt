c93e0fb322559a72b947934dde03383b9ddb47e4
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-11 21:36:22 +0200

    KS-3-1 Lesson 3 task 1

--- src/main/kotlin/lesson_3/lesson_3_task_1.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_1.kt
@@ -1,10 +1,9 @@
 package lesson_3
 fun main(){
-    val helloDay = "Добрый день"
-    val helloNight = "Доброй ночи"
+    val goodDay = "Добрый день"
+    val goodNight = "Доброй ночи"
     val user = "Пользователь"
-    var helloUser = helloDay + ", $user!"
-    println(helloUser)
-    helloUser = helloNight + ", $user!"
-    println(helloUser)
+
+    println("$goodDay, $user!")
+    println("$goodNight, $user!")
 }
