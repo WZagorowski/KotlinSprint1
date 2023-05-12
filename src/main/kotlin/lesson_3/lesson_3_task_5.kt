a2f8b163042c063c92a0491d7fb04a523c09bc58
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-12 17:11:19 +0200

    KS-3-5 Lesson 3 task 5

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -3,8 +3,7 @@ package lesson_3
 fun main(){
     val infoFromServer = "D2-D4;0"
 
-    val newInfo = infoFromServer.replace(";","-")
-    val result = newInfo.split("-")
+    val result = infoFromServer.split("-",";")
     val goFrom = result[0]
     val goTo = result[1]
     val moveNumber = result[2]
