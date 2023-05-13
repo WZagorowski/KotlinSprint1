3c00c9fe09e8c9316de76cf47855cccf94701f36
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-13 16:43:17 +0200

    KS-5-3 Lesson 5 task 3

---
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -0,0 +1,20 @@
+package lesson_5
+fun main(){
+    println("""
+    |Лотерея! Угадай оба числа и выйграй АВТОМОБИЛЬ!
+    |Введи два числа от 1 до 100 (через пробел в порядке возврастания)""".trimMargin())
+    val userNumbers = readLine()!!.split(" ")
+    val numberOne = userNumbers[0].toInt()
+    val numberTwo = userNumbers[1].toInt()
+    val result = if (numberOne == FIRST_WIN && numberTwo == SECOND_WIN)
+        "Поздравляем! Вы выиграли главный приз!"
+    else if (numberOne == FIRST_WIN || numberTwo == SECOND_WIN ||
+        numberOne == SECOND_WIN || numberTwo == FIRST_WIN)
+        "Вы выиграли утешительный приз!"
+    else "Неудача! Крутите барабан!"
+    println(result)
+    println("Числа, необходимые для победы: $FIRST_WIN $SECOND_WIN")
+}
+
+const val FIRST_WIN = 5
+const val SECOND_WIN = 25
