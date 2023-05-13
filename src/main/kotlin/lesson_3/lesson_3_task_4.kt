e0a4b606cd84c8265f335dc4f45eba8ac3e068f6
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-13 13:37:04 +0200

    KS-3-4 Lesson 3 task 4

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -1,12 +1,13 @@
 package lesson_3
 
-fun  main(){
+fun  main() {
     var goFrom = "E2"
     var goTo = "E4"
     var moveNumber = 1
-    println("Go from $goFrom to $goTo, move number $moveNumber")
+
+    println("$goFrom-$goTo;$moveNumber")
     goFrom = "D2"
-    goTo = "D3"
+    goTo = "D${2+1}"
     moveNumber += 1
-    println("Go from $goFrom to $goTo, move number $moveNumber")
+    println("$goFrom-$goTo;$moveNumber")
 }
