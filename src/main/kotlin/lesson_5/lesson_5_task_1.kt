fb3764b3038398aa16bdfe8a76f0799ca64d4fb3
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-14 00:25:47 +0200

    KS-5-1 Lesson 5 task 1

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -4,10 +4,12 @@ fun main(){
     val numberOne = 7
     val numberTwo = 8
     println("Введите результат сложения чисел. $numberOne + $numberTwo =")
-    val result = readLine()!!.toInt()
-
-    if (result == numberOne + numberTwo)
-        println("Добро пожаловать!")
-    else
-        println("Доступ запрещен.")
+    val result = readln().toIntOrNull()
+    if (result == null) println("Введен неправильный тип данных")
+    else {
+        if (result == numberOne + numberTwo)
+            println("Добро пожаловать!")
+        else
+            println("Доступ запрещен.")
+    }
 }
