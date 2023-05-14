89354cad8059ef196ca3bc61d2dd8542bf73045f
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-14 17:13:56 +0200

    KS-4-5 Lesson 4 task 5

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -1,17 +1,16 @@
 package lesson_4
 fun main(){
-    println("Есть ли повреждения корпуса? (да/нет/незначительные)")
-    val shipCondition = readLine()
-    val isShipConditionOk = shipCondition == "нет" || shipCondition == "незначительные"
+    println("Укажите состояние корпуса (введите 'true' если повреждений нет либо незначительные, 'false' если есть)")
+    val isShipConditionOk = readln().toBoolean()
 
     println("Текущая численность экипажа (введите число):")
-    val isPersonalOk = readLine()!!.toInt() in 56..70
+    val isPersonalOk = readln().toInt() in 56..70
 
     println("Текущее количество ящиков провизии (введите число):")
-    val isFoodOk = readLine()!!.toInt() > 50
+    val isFoodOk = readln().toInt() > 50
 
-    println("Благоприятные ли метеоусловия? (да/нет)")
-    val isWeatherOk = readLine() == "да"
+    println("Благоприятные ли метеоусловия? (введите 'true' если благоприятные, 'false' если нет)")
+    val isWeatherOk = readln().toBoolean()
 
     val result = isShipConditionOk && isPersonalOk && isFoodOk && isWeatherOk
     println("Условия для отправления корабля выполнены? $result")
