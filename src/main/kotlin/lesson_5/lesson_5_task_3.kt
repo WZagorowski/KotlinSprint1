bbf2e4b58965b3f8922785898effebd63509eb88
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-14 17:55:02 +0200

    KS-5-3 Lesson 5 task 3

--- src/main/kotlin/lesson_5/lesson_5_task_3.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_3.kt
@@ -3,15 +3,20 @@ fun main(){
     println("""
     |Лотерея! Угадай оба числа и выйграй АВТОМОБИЛЬ!
     |Введи два числа от 1 до 100 (через пробел в порядке возврастания)""".trimMargin())
-    val userNumbers = readLine()!!.split(" ")
+
+    val userNumbers = readln().split(" ")
     val numberOne = userNumbers[0].toInt()
     val numberTwo = userNumbers[1].toInt()
-    val result = if (numberOne == FIRST_WIN && numberTwo == SECOND_WIN)
-        "Поздравляем! Вы выиграли главный приз!"
-    else if (numberOne == FIRST_WIN || numberTwo == SECOND_WIN ||
-        numberOne == SECOND_WIN || numberTwo == FIRST_WIN)
-        "Вы выиграли утешительный приз!"
-    else "Неудача! Крутите барабан!"
+
+    val result =
+        if (numberOne == FIRST_WIN && numberTwo == SECOND_WIN)
+            "Поздравляем! Вы выиграли главный приз!"
+        else if (numberOne == FIRST_WIN || numberTwo == SECOND_WIN || numberOne == SECOND_WIN
+            || numberTwo == FIRST_WIN)
+            "Вы выиграли утешительный приз!"
+        else
+            "Неудача! Крутите барабан!"
+
     println(result)
     println("Числа, необходимые для победы: $FIRST_WIN $SECOND_WIN")
 }
