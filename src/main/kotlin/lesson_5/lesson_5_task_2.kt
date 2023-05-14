78526fa164afe61ae64c5a02e5d7c62394f2b916
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-14 17:38:08 +0200

    KS-5-2 Lesson 5 task 2

--- src/main/kotlin/lesson_5/lesson_5_task_2.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_2.kt
@@ -1,9 +1,11 @@
 package lesson_5
 fun main(){
     println("Введите год вашего рождения")
-    val userAge = 2023 - readLine()!!.toInt()
-    val resultText = if (userAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
-    else "Вернуться на главный экран"
+    val userAge = 2023 - readln().toInt()
+
+    val resultText =
+        if (userAge >= AGE_OF_MAJORITY) "Показать экран со скрытым контентом"
+        else "Вернуться на главный экран"
     println(resultText)
 }
 
