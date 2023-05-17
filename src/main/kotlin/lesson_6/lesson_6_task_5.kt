c31e4453c2f0b1361e64adeaba3ef58b2215b904
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-17 17:28:58 +0200

    KS-6-5 Lesson 6 task 5

---
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -0,0 +1,26 @@
+package lesson_6
+
+fun  main() {
+
+    var counter = 3
+
+    while (counter > 0) {
+
+        val numberOne = (1..9).random()
+        val numberTwo = (1..9).random()
+
+        println("Введите сумму чисел $numberOne и $numberTwo")
+
+        if (readln().toInt() == numberOne + numberTwo) {
+            println("Добро пожаловать!")
+            break
+        }
+        else if (counter > 1)
+            println("Ошибка! Попробуйте еще раз. Осталось попыток: ${--counter}")
+
+        else {
+            println("Доступ запрещен!")
+            break
+        }
+    }
+}
