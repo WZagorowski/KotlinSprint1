7ff6e2f6aa2acc0be9502acef366bf1e698e7994
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-18 20:35:49 +0200

    KS-4-3 Ввел константы для бобовых и сравнил их с заданными в задаче условиями

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,11 +1,19 @@
 package lesson_4
 
 fun main(){
-    val isWeatherSunny = true
-    val isTentOpen = true
-    val isHumidityCorrect = true
-    val isItNotWinter = false
 
-    val result = isWeatherSunny && isTentOpen && isHumidityCorrect && isItNotWinter
-    println("Благоприятные ли условия сейчас для роста бобовых? $result")
+    val isSunny = true
+    val isTentOpen = true
+    val humidity = 20
+    val isWinter = true
+
+    val resultForBeans = (isSunny == IS_SUNNY_FOR_BEANS) && (isTentOpen == IS_TENT_OPEN_FOR_BEANS) &&
+            (humidity == HUMIDITY_FOR_BEANS) && (isWinter == IS_WINTER_FOR_BEANS)
+
+    println("Благоприятные ли условия сейчас для роста бобовых? $resultForBeans")
 }
+
+const val IS_SUNNY_FOR_BEANS = true
+const val IS_TENT_OPEN_FOR_BEANS = true
+const val HUMIDITY_FOR_BEANS = 20
+const val IS_WINTER_FOR_BEANS = false
