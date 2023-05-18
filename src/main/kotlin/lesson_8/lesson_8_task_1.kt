62281d78fd66fa3c0da2bfb52e6d6028f7f1a822
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-18 22:00:10 +0200

    KS-8-1 Сложение элементов массива

--- src/main/kotlin/lesson_8/lesson_8_task_1.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_1.kt
@@ -1,7 +1,6 @@
 package lesson_8
 
 fun main() {
-    println("Сюда приходит количество просмотров за неделю по дням:")
 
     val viewsPerWeek = intArrayOf(11, 0, 39, 42, 31, 105, 48)
     var result = 0
