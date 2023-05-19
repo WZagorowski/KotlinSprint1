e9068b475a07eafb4721cfcfcb4283e176f1f086
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-19 22:00:45 +0200

    KS-8-5 убрал лишнее

--- src/main/kotlin/lesson_8/lesson_8_task_5.kt
+++ src/main/kotlin/lesson_8/lesson_8_task_5.kt
@@ -4,15 +4,15 @@ fun main() {
 
     println("Введите количество ингредиентов в списке:")
 
-    val quantity = readln().toInt()
-    val arrayOfIngredients = Array(quantity, {" "})
+    val arrayOfIngredients = Array(readln().toInt(), {" "})
 
-    for (i in 1..quantity) {
-        println("Введите ингредиент №$i:")
-        val ingredient = readln()
-        arrayOfIngredients[i-1] = ingredient
+    for (i in arrayOfIngredients.indices) {
+
+        println("Введите ингредиент №${i+1}:")
+        arrayOfIngredients[i] = readln()
     }
     println("Готово! Вы создали следующий список ингредиентов:")
+
     for (i in arrayOfIngredients)
         println(i)
 }
