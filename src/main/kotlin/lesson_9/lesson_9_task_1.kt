22ad81945600ede6864c82d67fe39a9bdf0241d8
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-20 19:52:02 +0200

    KS-9-1 вывод в консоль из списка

---
+++ src/main/kotlin/lesson_9/lesson_9_task_1.kt
@@ -0,0 +1,10 @@
+package lesson_9
+
+fun main() {
+
+    val dishIngredients = listOf("вода", "макарошки", "соль")
+    println("В рецепте есть следующие ингредиенты:")
+
+    for (i in dishIngredients)
+    println(i)
+}
