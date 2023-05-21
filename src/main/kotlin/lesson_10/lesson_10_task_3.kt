6b1b0160446341aeff0ce21f66b68cd62a340d7a
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-21 20:43:25 +0200

    KS-10-3 исправленная версия

--- src/main/kotlin/lesson_10/lesson_10_task_3.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_3.kt
@@ -1,13 +1,18 @@
 package lesson_10
+
 fun main() {
 
     println("Укажите длину вашего случайного пароля:")
     val passwordLength = readln().toInt()
+    val password = randomChar(passwordLength)
+
+    println("Ваш пароль готов:\n$password")
+}
+
+fun randomChar(passwordLength: Int): String {
     var password = ""
 
     for (i in 1..passwordLength)
-        password += randomChar()
-
-    println(password)
+        password += "0123456789!\"#\$%&'()*+,-./ ".random()
+    return password
 }
-fun randomChar(): Char = "0123456789!\"#\$%&'()*+,-./ ".random()
