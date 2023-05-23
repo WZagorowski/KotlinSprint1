186b9d64770c9cb0e10569b85eb5800ff6b8d5a3
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-24 00:33:08 +0200

    KS-10-4 доработал немного

--- src/main/kotlin/lesson_10/lesson_10_task_4.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_4.kt
@@ -2,39 +2,42 @@ package lesson_10
 
 fun main() {
 
+    println("Игра против компьютера в кости. Побеждает выбросивший наибольшее число. Стартуем!")
     var humanWins = 0
 
-    while (true) {
-        humanWins = newRound(humanWins)
-
+    do {
+        humanWins = getNewRound(humanWins)
         println("Хотите бросить кости еще раз (да/нет)?")
 
-        if (readln() == "нет") {
-            println("Человек выйграл партий: $humanWins")
-            break
-        }
-    }
+    } while (readln() == "да")
+
+        println("Человек выйграл партий: $humanWins")
 }
 
-fun newRound(humanWinsBefore: Int): Int {
+fun getNewRound(humanWinsBefore: Int): Int {
 
-    var humanWinsAfter = humanWinsBefore
+    val humanResult = getShootDice()
+    println("Человек выбросил $humanResult")
 
-    val humanBones = bones()
-    println("Человек выбросил $humanBones")
+    val computerResult = getShootDice()
+    println("Компьютер выбросил $computerResult")
 
-    val computerBones = bones()
-    println("Компьютер выбросил $computerBones")
+    return humanWinsBefore + compareResults(humanResult, computerResult)
+}
 
-    if (humanBones > computerBones) {
+fun getShootDice(): Int = (1..6).random() + (1..6).random()
+
+fun compareResults(humanResult: Int, computerResult: Int): Int {
+
+    var counter = 0
+
+    if (humanResult > computerResult) {
         println("Победил человек")
-        humanWinsAfter += 1
-    }
-    else if (humanBones == computerBones)
+        counter += 1
+    } else if (humanResult == computerResult)
         println("Ничья")
     else
         println("Победил компьютер")
-    return humanWinsAfter
-}
 
-fun bones(): Int = (1..6).random() + (1..6).random()
+    return counter
+}
