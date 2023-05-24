ab80520bb6b44632e84362e62823fc94a72bafbc
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-24 20:00:29 +0200

    KS-9-5 now with toList()

--- src/main/kotlin/lesson_9/lesson_9_task_5.kt
+++ src/main/kotlin/lesson_9/lesson_9_task_5.kt
@@ -2,19 +2,15 @@ package lesson_9
 
 fun main() {
 
-    val arrayOfIngredients = mutableSetOf<String>()
-    val listOfIngredients = mutableListOf<String>()
-
     println("Введите 5 ингредиентов, каждый с новой строки:")
+
+    val arrayOfIngredients = mutableSetOf<String>()
 
     for (i in 1..5) {
         arrayOfIngredients.add(readln())
     }
 
-    arrayOfIngredients.forEach() { it ->
-        listOfIngredients.add(it)
-    }
-
+    val listOfIngredients = (arrayOfIngredients.toList() as MutableList<String>)
     listOfIngredients.sort()
 
     listOfIngredients[0] = listOfIngredients[0].replaceFirstChar{it.uppercase()}
