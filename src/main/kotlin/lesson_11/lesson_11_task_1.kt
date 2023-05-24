5bdf9a5f79ba2c8f9010744d73fe15f1851b15dd
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-05-24 23:52:45 +0200

    KS-11-1 исправил

--- src/main/kotlin/lesson_11/lesson_11_task_1.kt
+++ src/main/kotlin/lesson_11/lesson_11_task_1.kt
@@ -1,1 +1,36 @@
 package lesson_11
+
+fun main() {
+
+    val userOne = User(
+        id = 1111,
+        login = "Чебурашка",
+        password = "1234e",
+        email = "cheburator123@gmail.com",
+    )
+
+    val userTwo = User(
+        id = 1110,
+        login = "Крокодил",
+        password = "777GENA",
+        email = "gennadiy77@krokodile.com",
+    )
+
+    println("ID ${userOne.id}")
+    println("Login ${userOne.login}")
+    println("Password ${userOne.password}")
+    println("Email ${userOne.email}")
+
+    println("\nID ${userTwo.id}")
+    println("Login ${userTwo.login}")
+    println("Password ${userTwo.password}")
+    println("Email ${userTwo.email}")
+}
+
+class User(
+    val id: Int,
+    val login: String,
+    val password: String,
+    val email: String,
+) {
+}
