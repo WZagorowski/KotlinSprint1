a3f3cdc3560e19835894902144dde5ebaa1125a2
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-06 21:25:35 +0200

    KS-11-3 Комнаты. + пришлось переименовать 2 функции в прошлом уроке

--- src/main/kotlin/lesson_10/lesson_10_task_2.kt
+++ src/main/kotlin/lesson_10/lesson_10_task_2.kt
@@ -4,14 +4,14 @@ fun main() {
 
     do {
         println("Username and password must be longer than 3 symbols.\nEnter your Username:")
-        val isUserNameOk = validateInputLength(readln())
+        val isUserNameOk = validateInputLength1(readln())
 
         println("Enter your Password:")
-        val isUserPasswordOk = validateInputLength(readln())
+        val isUserPasswordOk = validateInputLength1(readln())
     }
     while (!isUserNameOk || !isUserPasswordOk)
 
     println("Welcome!")
 }
 
-fun validateInputLength(userData: String): Boolean = userData.lastIndex >= 3
+fun validateInputLength1(userData: String): Boolean = userData.lastIndex >= 3
---
+++ src/main/kotlin/lesson_11/lesson_11_task_3.kt
@@ -0,0 +1,41 @@
+package lesson_11
+
+fun main() {
+
+    val room1 = Room(
+        cover = "image111",
+        name = "Барса",
+        membersAvatars = listOf("Image1001", "Image1002", "Image 1003", "Image 1004"),
+    )
+
+    val room2 = Room(
+        cover = "image112",
+        name = "Реал",
+        membersAvatars = listOf("Image2001", "Image2002", "Image 2003"),
+    )
+
+}
+
+class Room(
+    val cover: String,
+    val name: String,
+    val membersAvatars: List<String>,
+    ) {
+
+    fun tapToMemberAvatar(memberAvatar: String) {
+        println("Показать никнейм пользователя $memberAvatar")
+        showMemberStatus()
+    }
+
+    private fun showMemberStatus() {
+        val memberStatus = when (getRandomStatus()) {
+            0 -> "разговаривает"
+            1 -> "микрофон выключен"
+            else -> "пользователь заглушен"
+        }
+        println("Статус: $memberStatus")
+    }
+
+    private fun getRandomStatus(): Int = (0..2).random()
+
+}
