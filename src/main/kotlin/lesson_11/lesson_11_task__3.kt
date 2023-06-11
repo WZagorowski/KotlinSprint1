25258b0440e6c0888901885b6b0561a51f1ba4c2
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-11 16:59:43 +0200

    KS-11-3 выделил класс Member

--- src/main/kotlin/lesson_11/lesson_11_task__3.kt
+++ src/main/kotlin/lesson_11/lesson_11_task__3.kt
@@ -2,40 +2,62 @@ package lesson_11
 
 fun main() {
 
+    val member001 = Room.Member("001.jpg","Messi",)
+    val member002 = Room.Member("002.jpg", "Alba",)
+    val member003 = Room.Member("003.jpg", "Dembele",)
+
+    val member101 = Room.Member("101.jpg", "Benzema",)
+    val member102 = Room.Member("102.jpg", "Modric",)
+
     val room1 = Room(
         cover = "image111",
         name = "Барса",
-        membersAvatars = listOf("Image1001", "Image1002", "Image 1003", "Image 1004"),
+        members = mutableListOf(member001,member002,member003),
     )
 
     val room2 = Room(
         cover = "image112",
         name = "Реал",
-        membersAvatars = listOf("Image2001", "Image2002", "Image 2003"),
+        members = mutableListOf(member101,member102),
     )
 
+    room1.showMembersInTheRoom()
+    member001.tapToMemberAvatar()
+    member002.tapToMemberAvatar()
+    member003.tapToMemberAvatar()
 }
 
 private class Room(
     val cover: String,
     val name: String,
-    val membersAvatars: List<String>,
+    val members: MutableList<Member>,
 ) {
 
-    fun tapToMemberAvatar(memberAvatar: String) {
-        println("Показать никнейм пользователя $memberAvatar")
-        showMemberStatus()
+    fun showMembersInTheRoom() {
+        println("В комнате \"$name\" видим картинки пользователей:")
+        for (member in members)
+            println(member.memberAvatar)
     }
 
-    private fun showMemberStatus() {
-        val memberStatus = when (getRandomStatus()) {
-            0 -> "разговаривает"
-            1 -> "микрофон выключен"
-            else -> "пользователь заглушен"
+    class Member (
+        val memberAvatar: String,
+        val memberName: String,
+    ) {
+
+        fun tapToMemberAvatar() {
+            println("\nНажали на картинку пользователя $memberAvatar -> его никнейм: $memberName")
+            showMemberStatus()
         }
-        println("Статус: $memberStatus")
+
+        private fun showMemberStatus() {
+            val memberStatus = when (getRandomStatus()) {
+                0 -> "разговаривает"
+                1 -> "микрофон выключен"
+                else -> "пользователь заглушен"
+            }
+            println("Статус: $memberStatus")
+        }
+
+        private fun getRandomStatus(): Int = (0..2).random()
     }
-
-    private fun getRandomStatus(): Int = (0..2).random()
-
 }
