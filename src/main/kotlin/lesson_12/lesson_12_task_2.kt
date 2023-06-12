9e7cd728d1eec76c819557c202a1043988d218d6
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-12 21:46:06 +0200

    KS-12-2 Первичный конструктор

--- src/main/kotlin/lesson_12/lesson_12_task_2.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_2.kt
@@ -2,13 +2,13 @@ package lesson_12
 
 fun main() {
 
-    val weather11062023 = WeatherData(+21,+11,false,753.25,)
-    val weather12062023 = WeatherData(+23,+10,false,754.29,)
-    val weather13062023 = WeatherData(+18, +9,true, 753.45,)
+    val weather110623 = WeatherData(+21,+11,false,753.25,)
+    val weather120623 = WeatherData(+23,+10,false,754.29,)
+    val weather130623 = WeatherData(+18, +9,true, 753.45,)
 
-    weather11062023.outputWeatherData()
-    weather12062023.outputWeatherData()
-    weather13062023.outputWeatherData()
+    weather110623.outputWeatherData()
+    weather120623.outputWeatherData()
+    weather130623.outputWeatherData()
 }
 
 private class WeatherData(dayTemperature: Int, nightTemperature: Int, isItRaining: Boolean, atmospherePressure: Double,) {
