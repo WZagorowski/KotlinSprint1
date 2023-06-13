9b4f1211b2b35d9dee5e567144c764e5cf04d117
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-14 00:27:05 +0200

    KS-12-1 Пустой конструктор без +

--- src/main/kotlin/lesson_12/lesson_12_task_1.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_1.kt
@@ -3,14 +3,14 @@ package lesson_12
 fun main() {
 
     val weather12062023 = WeatherData()
-    weather12062023.dayTemperature = +23
-    weather12062023.nightTemperature = +10
+    weather12062023.dayTemperature = 23
+    weather12062023.nightTemperature = 10
     weather12062023.isItRaining = false
     weather12062023.atmospherePressure = 754.29
 
     val weather13062023 = WeatherData()
-    weather13062023.dayTemperature = +18
-    weather13062023.nightTemperature = +9
+    weather13062023.dayTemperature = 18
+    weather13062023.nightTemperature = 9
     weather13062023.isItRaining = true
     weather13062023.atmospherePressure = 753.45
 
