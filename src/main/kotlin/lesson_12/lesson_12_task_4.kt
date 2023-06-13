314af964b90398fc8809c676724c20c2cf7a207f
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-14 00:52:52 +0200

    KS-12-4 исправил

--- src/main/kotlin/lesson_12/lesson_12_task_4.kt
+++ src/main/kotlin/lesson_12/lesson_12_task_4.kt
@@ -2,18 +2,25 @@ package lesson_12
 
 fun main() {
 
-    val weatherToday = WeatherData(+23,+10,754.29,)
-    val weatherTomorrow= WeatherData(+18, +9,753.45, true)
+    val weatherToday = WeatherData(23, 10, 754.29)
+    val weatherTomorrow = WeatherData(18, 9, 753.45, true)
 }
 
-private class WeatherData(val dayTemperature: Int, val nightTemperature: Int, val atmospherePressure: Double,
-                          val isItRaining: Boolean = false,) {
+private class WeatherData(
+    val dayTemperature: Int,
+    val nightTemperature: Int,
+    val atmospherePressure: Double,
+    val isItRaining: Boolean = false,
+    ) {
+
     init {
-        println("""
-            
+        println(
+            """
+                
             Температура днем: $dayTemperature
             Температура ночью: $nightTemperature
             Дождь в этот день: $isItRaining
-            Атмосферное давление: $atmospherePressure """.trimIndent())
+            Атмосферное давление: $atmospherePressure """.trimIndent()
+        )
     }
 }
