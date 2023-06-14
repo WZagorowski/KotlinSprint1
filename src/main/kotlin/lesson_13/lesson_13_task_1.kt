08e1519b3376c059cd0afc017de08b6bd5c02d37
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-14 23:22:18 +0200

    KS-13-1 все оказалось проще

--- src/main/kotlin/lesson_13/lesson_13_task_1.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_1.kt
@@ -3,12 +3,10 @@ package lesson_13
 fun main() {
 }
 
-class PhoneContact1(val name: String, val phoneNumber: Long) {
+private class PhoneContact1(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String?
+)
 
-    private var company: String = ""
 
-    constructor(name: String, phoneNumber: Long, company: String) : this(name, phoneNumber) {
-
-        this.company = company
-    }
-}
