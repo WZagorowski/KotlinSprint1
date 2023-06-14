77095326bd7b11b8e50a11ff8ab1366c83eff9de
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-14 23:28:44 +0200

    KS-13-2

--- src/main/kotlin/lesson_13/lesson_13_task_2.kt
+++ src/main/kotlin/lesson_13/lesson_13_task_2.kt
@@ -1,23 +1,13 @@
 package lesson_13
 
 fun main() {
-    val contact13 = PhoneContact2(
-        "Antony",
-        8911911555666,
-        null,
-    )
+
+    val contact13 = PhoneContact2("Antony", 8911911555666, null)
 
     contact13.outputPhoneContact()
 }
 
-class PhoneContact2(private val name: String, private val phoneNumber: Long) {
-
-    private var company: String? = null
-
-    constructor(name: String, phoneNumber: Long, company: String?) : this(name, phoneNumber) {
-
-        this.company = company
-    }
+private class PhoneContact2(val name: String, val phoneNumber: Long, val company: String?) {
 
     fun outputPhoneContact() {
         println("Имя: $name \nНомер: $phoneNumber \nКомпания: ${company ?: "не указано"}")
