73e058787ba0a1b3fc54322012b15cb33dceb4a6
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-15 23:12:25 +0200

    KS-14-1 Создать класс: Стандартный корабль

--- src/main/kotlin/lesson_14/lesson_14_task_1.kt
+++ src/main/kotlin/lesson_14/lesson_14_task_1.kt
@@ -3,17 +3,17 @@ package lesson_14
 fun main() {
 
     val ship1 = StandardShip1("Kraken", 15, 200)
-    ship1.showSpeed()
-    ship1.showCapacity()
+    ship1.printSpeed()
+    ship1.printCapacity()
 }
 
 class StandardShip1(private val name: String, private val speed: Int, private val capacity: Int) {
 
-    fun showSpeed() {
+    fun printSpeed() {
         println("Скорость корабля $name составляет: $speed узлов")
     }
 
-    fun showCapacity() {
+    fun printCapacity() {
         println("Вместимость корабля $name составляет: $capacity контейнеров")
     }
 }
