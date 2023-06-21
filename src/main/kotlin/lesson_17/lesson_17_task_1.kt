3c71eada170a8e3cfdd3df44ee4c86b042ef82ce
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-21 17:39:39 +0200

    KS-17-1 сеттер и геттер для викторины

---
+++ src/main/kotlin/lesson_17/lesson_17_task_1.kt
@@ -0,0 +1,15 @@
+package lesson_17
+
+private class Quiz() {
+
+    var question: String = ""
+        set(value) {
+            field = value
+        }
+
+    var answer: String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+}
