be1d3b6bd7eca6cb99495241bda978a4e040a70f
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-22 18:01:36 +0200

    KS-16-3 пароль в основном конструкторе

--- src/main/kotlin/lesson_16/lesson_16_task_3.kt
+++ src/main/kotlin/lesson_16/lesson_16_task_3.kt
@@ -1,26 +1,17 @@
 package lesson_16
 
 fun main() {
+    val user16 = User16("Вовочка", "123йцуке")
 
-    val user16 = User16("Вовочка")
     user16.checkPassword()
 }
 
-private class User16(val userName16: String) {
-
-    private var password16 = ""
-
-    init {
-        println("Для завершения регистрации установите пароль:")
-        password16 = readln()
-        println("Пароль для пользователя $userName16 успешно сохранен.")
-    }
+private class User16(val userName: String, private var password: String) {
 
     fun checkPassword() {
+        do println("Для входа введите ваш пароль:")
+        while (readln() != password)
 
-        do {
-            println("Для входа введите ваш пароль:")
-        } while (readln() != password16)
-        println("Добро пожаловать, $userName16!")
+        println("Добро пожаловать, $userName!")
     }
 }
