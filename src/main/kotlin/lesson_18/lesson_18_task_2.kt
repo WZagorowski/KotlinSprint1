dfd4e2061a1fdce4c83cb1ac5baf5d68617cfe0e
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-23 01:11:51 +0200

    KS-18-2 полиморфизм для 3 дочерних классов с полями одного типа

--- src/main/kotlin/lesson_18/lesson_18_task_2.kt
+++ src/main/kotlin/lesson_18/lesson_18_task_2.kt
@@ -2,9 +2,9 @@ package lesson_18
 
 fun main() {
 
-    val dice4 = FourSidesDice()
-    val dice6 = SixSidesDice()
-    val dice8 = EightSidesDice()
+    val dice4 = FourSidedDice()
+    val dice6 = SixSidedDice()
+    val dice8 = EightSidedDice()
 
     dice4.getShootingDice()
     dice6.getShootingDice()
@@ -16,8 +16,8 @@ abstract class AbstractDice(private val numberSide: Int) {
     fun getShootingDice() = println((1..numberSide).random())
 }
 
-private class FourSidesDice(numberSide: Int = 4) : AbstractDice(numberSide)
+private class FourSidedDice(numberSide: Int = 4) : AbstractDice(numberSide)
 
-private class SixSidesDice(numberSide: Int = 6) : AbstractDice(numberSide)
+private class SixSidedDice(numberSide: Int = 6) : AbstractDice(numberSide)
 
-private class EightSidesDice(numberSide: Int = 8) : AbstractDice(numberSide)
+private class EightSidedDice(numberSide: Int = 8) : AbstractDice(numberSide)
