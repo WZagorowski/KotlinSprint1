573582a988a58c11876220cc8da30165f467f0cc
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-24 22:25:36 +0200

    KS-19-1 создать класс ENUM

--- src/main/kotlin/lesson_19/lesson_19_task_1.kt
+++ src/main/kotlin/lesson_19/lesson_19_task_1.kt
@@ -1,2 +1,28 @@
 package lesson_19
 
+fun main() {
+
+    println("Список рыбок, доступных для добавления в аквариум:")
+    for (i in Fish.values()) println(i.getFishName())
+}
+
+enum class Fish {
+
+    GUPPY {
+        override fun getFishName() = "Гуппи"
+    },
+
+    ANGELFISH {
+        override fun getFishName() = "Скалярия"
+    },
+
+    GOLDFISH {
+        override fun getFishName() = "Золотая рыбка"
+    },
+
+    SIAMESE_FIGHTING_FISH {
+        override fun getFishName() = "Петушок"
+    };
+
+    abstract fun getFishName(): String
+}
