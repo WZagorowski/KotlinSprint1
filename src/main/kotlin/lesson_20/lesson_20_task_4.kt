a5acc579e1c3d90ae15cbf40bf57981b3cd78caf
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-26 23:40:23 +0200

    KS-20-4 список элементов заменить на список лямбд с помощью map()

--- src/main/kotlin/lesson_20/lesson_20_task_4.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_4.kt
@@ -2,12 +2,10 @@ package lesson_20
 
 fun main() {
     val listOfItems = listOf("red", "orange", "yellow", "green", "blue", "indigo", "violet")
-
     val resultList = listOfItems.filterIndexed { index, _ -> index % 2 == 1 }
 
-    resultList.map {
-        { it: String -> println("Нажат элемент $it") }(it)
-    }
+    val printItem: (String) -> Unit = { it: String -> println("Нажат элемент $it") }
 
+    resultList.map { it: String -> printItem(it) }
 }
 
