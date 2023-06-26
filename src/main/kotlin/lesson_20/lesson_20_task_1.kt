bd2bf2b8cbbb248dde12eea2afc7ce3ad5cf1f78
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-26 19:57:59 +0200

    KS-20-1 лямбда-функция создает строку

--- src/main/kotlin/lesson_20/lesson_20_task_1.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_1.kt
@@ -1,2 +1,13 @@
 package lesson_20
 
+fun main() {
+    val user01 = User("Дед Мороз")
+
+    val getSplashScreen: (String) -> String = { name: String ->
+        "С наступающим Новым Годом, $name!"
+    }
+
+    println(getSplashScreen(user01.userName))
+}
+
+private class User(val userName: String)
