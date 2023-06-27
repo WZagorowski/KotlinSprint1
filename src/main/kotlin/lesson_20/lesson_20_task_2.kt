9dfb0ab55b616707fce83604bacc5f6e4b96b5dc
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-27 20:28:39 +0200

    KS-20-2 перенес функции в класс

--- src/main/kotlin/lesson_20/lesson_20_task_2.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_2.kt
@@ -3,18 +3,20 @@ package lesson_20
 fun main() {
     val player20 = Player(name = "Валера", currentHealth = 25)
 
-    val getHealingPotion = { it: Player ->
-        it.currentHealth = it.maxHealth
-        println("Игрок ${player20.name} применил лечебное зелье.")
-    }
-
-    getHealingPotion(player20)
-
-    println("Текущее здоровье игрока ${player20.name}: ${player20.currentHealth} ед.")
+    player20.getHealingPotion()
+    player20.getHealthInfo()
 }
 
 private class Player(
     val name: String,
     var currentHealth: Int,
     val maxHealth: Int = 100,
-)
+) {
+
+    val getHealingPotion = {
+        currentHealth = maxHealth
+        println("Игрок $name применил лечебное зелье.")
+    }
+
+    val getHealthInfo = { println("Текущее здоровье игрока $name: $currentHealth ед.") }
+}
