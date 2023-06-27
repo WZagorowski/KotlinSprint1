7b068472bb64478c03541875f623a5ba6320778f
WZagorowski <vladislav.zagorovsky@gmail.com>
2023-06-27 20:48:53 +0200

    KS-20-3 перенес функции в класс Player

--- src/main/kotlin/lesson_20/lesson_20_task_3.kt
+++ src/main/kotlin/lesson_20/lesson_20_task_3.kt
@@ -4,20 +4,25 @@ fun main() {
     val player20 = Player(name = "Валера", isPlayerHaveKey = false)
     val player21 = Player(name = "Женя", isPlayerHaveKey = true)
 
-    val checkHavingKey = { it: Player ->
-        if (it.isPlayerHaveKey) {
-            println("Игрок ${it.name} торжественно открыл дверь.")
-            it.isPlayerHaveKey = false
-        } else
-            println("Игрок ${it.name} не может открыть дверь. Необходим ключик!")
-    }
-
-    checkHavingKey(player20)
-    checkHavingKey(player21)
-    checkHavingKey(player21)
+    player20.tryOpenDoor()
+    player21.tryOpenDoor()
 }
 
 private class Player(
     val name: String,
     var isPlayerHaveKey: Boolean,
-)
+) {
+
+    val tryOpenDoor = {
+        println("Игрок $name пытается открыть запертую дверь...")
+        checkHavingKey()
+    }
+
+    private val checkHavingKey = {
+        if (isPlayerHaveKey) {
+            println("У игрока $name оказался необходимый ключик и дверь отворилась.")
+            isPlayerHaveKey = false
+        } else
+            println("Игрок $name не может открыть дверь. Необходим ключик!")
+    }
+}
